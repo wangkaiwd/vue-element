555fc30d58ba694b13c2789309e62e3cfae8b8ed
wangkaiwd <1162486176@qq.com>
2018-08-22 19:09:12 +0800

    配置有赞ui库

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import Vant from 'vant';
+import 'vant/lib/vant-css/index.css';
+Vue.use(Vant);
 import '@/assets/styles/reset'
 import '@/utils/rem.js'
 Vue.config.productionTip = false
--- src/utils/rem.js
+++ src/utils/rem.js
@@ -1,10 +1,15 @@
+/**
+ *  动态设置根元素thml的font-size
+ * 这里： 1rem = 50px;
+ * 对应到二倍图设计稿： 页面元素大小 = 设计图元素尺寸 / 100 rem;
+ */
 (function (doc, win) {
   const html = document.documentElement;
   const setRem = () => {
     const { width } = html.getBoundingClientRect();
-    if (!width) { return }
+    if (!width) { return };
     html.style.fontSize = width / 7.5 + 'px';
   }
-  win.addEventListener('resize', setRem)
-  doc.addEventListener('DOMContentLoaded', setRem)
+  win.addEventListener('resize', setRem);
+  doc.addEventListener('DOMContentLoaded', setRem);
 }(document, window))
