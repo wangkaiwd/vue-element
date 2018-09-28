1cc1621987c81972602f7e6c3d98e1d38fe84c3a
wangkaiwd <1162486176@qq.com>
2018-09-28 19:25:54 +0800

    删除多余Log

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -7,34 +7,34 @@
 </template>
 
 <script>
-  import Header from './components/header/elHeader'
-  import Tabs from './components/tabs/tabs'
+import Header from './components/header/elHeader'
+import Tabs from './components/tabs/tabs'
 
-  export default {
-    name: 'home',
-    components: {
-      'el-header': Header,
-      'el-tabs': Tabs
-    },
-    data () {
-      return {
-        sellerData: {}
-      }
-    },
-    mounted () {
-      this.getSellerData()
-    },
-    methods: {
-      getSellerData () {
-        this.$api.element.fetchSeller({}, res => {
-          this.sellerData = res.seller
-        })
-      }
+export default {
+  name: 'home',
+  components: {
+    'el-header': Header,
+    'el-tabs': Tabs
+  },
+  data() {
+    return {
+      sellerData: {}
+    }
+  },
+  mounted() {
+    this.getSellerData()
+  },
+  methods: {
+    getSellerData() {
+      this.$api.element.fetchSeller({}, res => {
+        this.sellerData = res.seller
+      })
     }
   }
+}
 </script>
 
 <style lang="less" scoped>
-  .home {
-  }
+.home {
+}
 </style>
