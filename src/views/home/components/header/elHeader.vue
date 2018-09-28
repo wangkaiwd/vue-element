c16e65987a39e6f56631d9a5fe7edd698d3a2cc1
wangkaiwd <1162486176@qq.com>
2018-09-28 20:09:43 +0800

    头部模态框样式结构初步搭建

--- src/views/home/components/header/elHeader.vue
+++ src/views/home/components/header/elHeader.vue
@@ -27,161 +27,174 @@
     <div class="background">
       <img :src="sellerData.avatar" alt="">
     </div>
-    <el-modal v-show="modalVisible" :modal-visible.sync="modalVisible"></el-modal>
+    <el-modal :sellerData="sellerData" 
+              :supportsImgArray="supportsImgArray" 
+              v-show="modalVisible" 
+              :modal-visible.sync="modalVisible">
+    </el-modal>
   </div>
 </template>
 
 <script>
-  import BulletinPic from '@/assets/img/bulletin@2x.png'
-  import BrandPic from '@/assets/img/brand@2x.png'
-  import SupportsPic1 from '@/assets/img/decrease_1@2x.png'
-  import SupportsPic2 from '@/assets/img/discount_2@2x.png'
-  import SupportsPic3 from '@/assets/img/special_2@2x.png'
-  import SupportsPic4 from '@/assets/img/invoice_2@2x.png'
-  import SupportsPic5 from '@/assets/img/guarantee_2@2x.png'
-  import ElModal from '../modal/modal'
+import BulletinPic from '@/assets/img/bulletin@2x.png'
+import BrandPic from '@/assets/img/brand@2x.png'
+import SupportsPic1 from '@/assets/img/decrease_1@2x.png'
+import SupportsPic2 from '@/assets/img/discount_2@2x.png'
+import SupportsPic3 from '@/assets/img/special_2@2x.png'
+import SupportsPic4 from '@/assets/img/invoice_2@2x.png'
+import SupportsPic5 from '@/assets/img/guarantee_2@2x.png'
+import ElModal from '../modal/modal'
 
-  export default {
-    name: 'elHeader',
-    components: {ElModal},
-    props: {
-      sellerData: {
-        type: Object,
-        required: true
-      }
+export default {
+  name: 'elHeader',
+  components: { ElModal },
+  props: {
+    sellerData: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      BrandPic,
+      BulletinPic,
+      SupportsPic1,
+      SupportsPic2,
+      SupportsPic3,
+      SupportsPic4,
+      SupportsPic5,
+      modalVisible: false
+    }
+  },
+  computed: {
+    // 用索引和图片进行对应
+    supportsImg() {
+      const imgArr = [
+        this.SupportsPic1,
+        this.SupportsPic2,
+        this.SupportsPic3,
+        this.SupportsPic4,
+        this.SupportsPic5
+      ]
+      return imgArr[this.sellerData.supports[0].type]
     },
-    data () {
-      return {
-        BrandPic,
-        BulletinPic,
-        SupportsPic1,
-        SupportsPic2,
-        SupportsPic3,
-        SupportsPic4,
-        SupportsPic5,
-        modalVisible: false,
-      }
-    },
-    computed: {
-      // 用索引和图片进行对应
-      supportsImg () {
-        const imgArr = [
-          this.SupportsPic1,
-          this.SupportsPic2,
-          this.SupportsPic3,
-          this.SupportsPic4,
-          this.SupportsPic5
-        ]
-        return imgArr[this.sellerData.supports[0].type]
-      }
+    supportsImgArray() {
+      return [
+        this.SupportsPic1,
+        this.SupportsPic2,
+        this.SupportsPic3,
+        this.SupportsPic4,
+        this.SupportsPic5
+      ]
     }
   }
+}
 </script>
 
 <style lang="less" scoped>
-  @import '~styles/mixins.less';
+@import '~styles/mixins.less';
 
-  @header-bg-color: rgba(7, 17, 27, .5);
-  .header {
+@header-bg-color: rgba(7, 17, 27, 0.5);
+.header {
+  position: relative;
+  padding-top: 1px;
+  height: 2.68rem;
+  background-color: @header-bg-color;
+  color: #fff;
+  .header-content {
     position: relative;
-    padding-top: 1px;
-    height: 2.68rem;
-    background-color: @header-bg-color;
-    color: #fff;
-    .header-content {
-      position: relative;
-      margin-top: .48rem;
-      margin-left: .48rem;
-      display: flex;
-    }
-    .content-left {
-      width: 1.28rem;
-      height: 1.28rem;
-    }
-    .content-right {
-      margin-left: .32rem;
-    }
-    .title {
-      margin-top: .04rem;
-      font-size: 16px;
-      img {
-        width: .6rem;
-        height: .36rem;
-        margin-right: .12rem;
-      }
-    }
-    .deliver {
-      font-size: 14px;
-      margin-top: .16rem;
-    }
-    .full-cut {
-      display: flex;
-      font-size: 12px;
-      margin-top: .2rem;
-      > span {
-        display: inline-block;
-        width: .24rem;
-        height: .24rem;
-        margin-right: .08rem;
-      }
-    }
-    .supports-number {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      position: absolute;
-      right: .24rem;
-      bottom: -.12rem;
-      background-color: rgba(0, 0, 0, .2);
-      font-size: 12px;
-      width: .8rem;
-      height: .48rem;
-      border-radius: .16rem;
-    }
-
-    .header-bottom {
-      padding: 0 .24rem;
-      position: absolute;
-      left: 0;
-      bottom: 0;
-      width: 100%;
-      min-width: 0;
-      height: .56rem;
-      background-color: rgba(7, 17, 27, .2);
-      display: flex;
-      align-items: center;
-    }
-    .bulletin {
-      display: inline-block;
-      height: .30rem;
-      width: .50rem;
-      margin-right: .08rem;
-      > img {
-        width: 100%;
-        height: 100%;
-      }
-    }
-    .bulletin-text {
-      .ell();
-      font-size: 12px;
-      flex: 1;
-      min-width: 0;
-    }
-    .icon-right {
-      font-size: 12px;
-    }
-    .background {
-      position: absolute;
-      top: 0;
-      bottom: 0;
-      left: 0;
-      right: 0;
-      > img {
-        width: 100%;
-        height: 100%;
-      }
-      filter: blur(10px);
-      z-index: -1;
+    margin-top: 0.48rem;
+    margin-left: 0.48rem;
+    display: flex;
+  }
+  .content-left {
+    width: 1.28rem;
+    height: 1.28rem;
+  }
+  .content-right {
+    margin-left: 0.32rem;
+  }
+  .title {
+    margin-top: 0.04rem;
+    font-size: 16px;
+    img {
+      width: 0.6rem;
+      height: 0.36rem;
+      margin-right: 0.12rem;
     }
   }
+  .deliver {
+    font-size: 14px;
+    margin-top: 0.16rem;
+  }
+  .full-cut {
+    display: flex;
+    font-size: 12px;
+    margin-top: 0.2rem;
+    > span {
+      display: inline-block;
+      width: 0.24rem;
+      height: 0.24rem;
+      margin-right: 0.08rem;
+    }
+  }
+  .supports-number {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    right: 0.24rem;
+    bottom: -0.12rem;
+    background-color: rgba(0, 0, 0, 0.2);
+    font-size: 12px;
+    width: 0.8rem;
+    height: 0.48rem;
+    border-radius: 0.16rem;
+  }
+
+  .header-bottom {
+    padding: 0 0.24rem;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    min-width: 0;
+    height: 0.56rem;
+    background-color: rgba(7, 17, 27, 0.2);
+    display: flex;
+    align-items: center;
+  }
+  .bulletin {
+    display: inline-block;
+    height: 0.3rem;
+    width: 0.5rem;
+    margin-right: 0.08rem;
+    > img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+  .bulletin-text {
+    .ell();
+    font-size: 12px;
+    flex: 1;
+    min-width: 0;
+  }
+  .icon-right {
+    font-size: 12px;
+  }
+  .background {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    > img {
+      width: 100%;
+      height: 100%;
+    }
+    filter: blur(10px);
+    z-index: -1;
+  }
+}
 </style>
--- src/views/home/components/modal/modal.vue
+++ src/views/home/components/modal/modal.vue
@@ -3,18 +3,32 @@
     <div class="modal-content">
       <div class="content-top">
         <div class="title">
-          <h3>粥品香坊（大运村）</h3>
+          <h3>{{sellerData.name}}</h3>
         </div>
         <div class="rating">
-
+          评分
         </div>
       </div>
       <div class="content-middle">
         <div class="favourable">
           <div class="content-title">
-            <div class="title-left"></div>
+            <div class="title-left">
+              <p class="line"></p>
+            </div>
             <div class="title-text">优惠信息</div>
-            <div class="title-right"></div>
+            <div class="title-right">
+              <p class="line"></p>
+            </div>
+          </div>
+          <div class="content-list">
+            <ul>
+              <li class="list-item" 
+                  v-for="(item,index) in sellerData.supports" 
+                  :key="index">
+                <img :src="supportsImgArray[item.type]" alt="">
+                <p>{{item.description}}</p>
+              </li>
+            </ul>
           </div>
         </div>
         <div class="notice"></div>
@@ -32,42 +46,69 @@
 </template>
 
 <script>
-  export default {
-    name: 'modal',
-    methods: {}
-  }
+export default {
+  name: 'modal',
+  props: {
+    sellerData: {
+      type: Object,
+      required: true
+    },
+    supportsImgArray: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {}
+}
 </script>
 
 <style lang="less" scoped>
-  .modal {
-    position: fixed;
-    left: 0;
-    top: 0;
-    height: 100vh;
-    width: 100%;
-    background-color: rgba(7, 17, 27, .8);
-    .modal-content {
-      height: 100%;
-      padding: 1.28rem .72rem .64rem;
+.modal {
+  position: fixed;
+  left: 0;
+  top: 0;
+  height: 100vh;
+  width: 100%;
+  background-color: rgba(7, 17, 27, 0.8);
+  .modal-content {
+    height: 100%;
+    padding: 1.28rem 0.72rem 0.64rem;
+  }
+  .close {
+    text-align: center;
+  }
+  .title {
+    font-size: 16px;
+  }
+  .rating {
+    margin-bottom: 0.56rem;
+    margin-top: 0.32rem;
+  }
+  .close-icon {
+    color: rgba(255, 255, 255, 0.5);
+    font-size: 32px;
+  }
+  .content-title {
+    display: flex;
+    .title-left,
+    .title-right {
+      width: 2.24rem;
+      display: flex;
+      align-items: center;
+      justify-content: center;
     }
-    .close {
+    .title-text {
+      flex: 1;
       text-align: center;
     }
-    .close-icon {
-      color: rgba(255, 255, 255, .5);
-      font-size: 32px;
+    .line {
+      width: 100%;
+      height: 2px;
+      background-color: rgba(255, 255, 255, 0.2);
     }
-    .content-title {
-      display: flex;
-      .title-left,
-      .title-right {
-        width: .224rem;
-        color: rgba(255, 255, 255, .2);
-      }
-      .title-text {
-        flex: 1;
-        text-align: center;
-      }
+    .content-list {
+      padding: 0.48rem 0.24rem 0.56rem;
     }
   }
+}
 </style>
