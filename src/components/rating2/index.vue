23e798d6dfbdbb03f2909698bd8026bd6ee170fd
wangkaiwd <1162486176@qq.com>
2018-10-01 23:24:04 +0800

    优化rating组件的代码结构

--- src/components/rating2/index.vue
+++ src/components/rating2/index.vue
@@ -48,71 +48,37 @@
 
 <style lang="less" scoped>
   .rating2 {
-    display: inline-flex;
-    font-size: 0;
-    span {display: inline-block;}
+    display: inline-flex;font-size: 0;
+    span {display: inline-block;
+      &:last-child {margin-right: 0;}
+    }
   }
 
   .star-48 {
     .star-item {
-      width: 20px;
-      height: 20px;
-      margin-right: 22px;
-      background-size: 20px 20px;
-      &:last-child {
-        margin-right: 0;
-      }
-      &.on {
-        background-image: url('~@/assets/img/star48_on@2x.png');
-      }
-      &.half {
-        background-image: url('~@/assets/img/star48_half@2x.png');
-      }
-      &.off {
-        background-image: url('~@/assets/img/star48_off@2x.png');
-      }
+      width: 20px;height: 20px;margin-right: 22px;background-size: 20px 20px;
+      &.on {background-image: url('~@/assets/img/star48_on@2x.png');}
+      &.half {background-image: url('~@/assets/img/star48_half@2x.png');}
+      &.off {background-image: url('~@/assets/img/star48_off@2x.png');}
     }
   }
 
   .star-36 {
     .star-item {
-      width: 15px;
-      height: 15px;
-      margin-right: 3px;
-      background-size: 15px 15px;
-      &:last-child {
-        margin-right: 0;
-      }
-      &.on {
-        background-image: url('~@/assets/img/star36_on@2x.png');
-      }
-      &.half {
-        background-image: url('~@/assets/img/star36_half@2x.png');
-      }
-      &.off {
-        background-image: url('~@/assets/img/star36_off@2x.png');
-      }
+      width: 15px;height: 15px;margin-right: 3px;background-size: 15px 15px;
+      &.on {background-image: url('~@/assets/img/star36_on@2x.png');}
+      &.half {background-image: url('~@/assets/img/star36_half@2x.png');}
+      &.off {background-image: url('~@/assets/img/star36_off@2x.png');}
     }
   }
 
   .star-24 {
     .star-item {
-      width: 10px;
-      height: 10px;
-      margin-right: 3px;
-      background-size: 10px 10px;
-      &:last-child {
-        margin-right: 0;
-      }
-      &.on {
-        background-image: url('~@/assets/img/star24_on@2x.png');
-      }
-      &.half {
-        background-image: url('~@/assets/img/star24_half@2x.png');
-      }
-      &.off {
-        background-image: url('~@/assets/img/star24_off@2x.png');
-      }
+      width: 10px;height: 10px;margin-right: 3px;background-size: 10px 10px;
+      &:last-child {margin-right: 0;}
+      &.on {background-image: url('~@/assets/img/star24_on@2x.png');}
+      &.half {background-image: url('~@/assets/img/star24_half@2x.png');}
+      &.off {background-image: url('~@/assets/img/star24_off@2x.png');}
     }
   }
 </style>
