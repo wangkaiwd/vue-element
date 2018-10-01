ae3d09fa901f09b6fc717e376d85b9d4bcac7e24
wangkaiwd <1162486176@qq.com>
2018-10-01 20:18:48 +0800

    想要将骨架屏添加为全局loading，效果并不好

--- src/request/axiosConfig.js
+++ src/request/axiosConfig.js
@@ -1,6 +1,5 @@
 import axios from 'axios'
 import qs from 'qs'
-import { Toast } from 'vant'
 import store from '@/store'
 
 import httpServer from './serverConfig'
@@ -10,20 +9,15 @@ const instance = axios.create({
   baseURL: httpServer.mockURL,
   timeout: 20000
 })
-instance.defaults.headers.post['Content-Type'] =
-  'application/x-www-form-urlencoded;charset=UTF-8'
+instance.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
 instance.interceptors.request.use(
   config => {
-    const { method, data } = config
+    const {method, data} = config
     if (method === 'post') {
       config.data = qs.stringify(data)
     }
     const token = getToken()
     token && (config.headers['user_token'] = token)
-    Toast.loading({
-      mask: true,
-      message: '加载中...'
-    })
     return config
   },
   error => {
@@ -33,7 +27,6 @@ instance.interceptors.request.use(
 instance.interceptors.response.use(
   response => {
     store.commit('changeWebStatus', false)
-    Toast.clear()
     if (response.status === 200) {
       return response.data
     } else {
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,21 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 const store = new Vuex.Store({
   state: {
-    offline: false
+    offline: false,
+    loading: false
   },
   mutations: {
     changeWebStatus (state, status) {
-      state.offline = status;
-      return state;
+      state.offline = status
+      return state
+    },
+    changeLoading (state, status) {
+      state.loading = status
     }
   }
-});
+})
 
-export default store;
+export default store
